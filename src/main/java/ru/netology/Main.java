adb574f77e4fa869cd726ac6c1cfb550a6a8ddb8
Vyacheslav Vyatkin <Vyatkin.vyacheslav.a@yandex.ru>
2023-07-12 21:19:24 +0500

    initial commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4516326598";
+        post.patronymic = "Сергеевич";
+        post.phone = "+7 - 912 - 555 - 55 - 55";
+        post.surname = "Петров";
+        post.subscription = true;
+        post.birthday = FormDate;
+
+
+    }
+}
